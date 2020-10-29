07a85b4212daa9dbe7669aa3c1a8225f0270a4f1
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-29 18:57:10 +0530

    source data staging changes

---
+++ com/pg/source_data_loading.py
@@ -0,0 +1,89 @@
+from pyspark.sql import SparkSession
+import yaml
+import os.path
+
+from sqlalchemy.sql.functions import current_date
+
+import com.pg.utils.utility as ut
+
+if __name__ == '__main__':
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    src_list = app_conf["source_list"]
+
+    for src in src_list:
+        src_conf = app_conf[src]
+
+        if src == 'SB':
+            # Read data from MySQL - TransactionSync table, create dataframe out of it
+            # Add a column 'ins_dt' - current_date()
+            # Write dataframe in S3 partitioned by 'ins_dt'
+
+            txn_df = ut.read_from_mysql(spark, src_conf, app_secret) \
+                .withColumn("ins_dt", current_date())
+
+            txn_df.show()
+
+            txn_df.write \
+                .partitionedBy("ins_dt") \
+                .parquet("s3:a//" + app_conf["s3_conf"]["s3_bucket"]+"/"+src)
+
+        elif src == 'OL':
+            # Read data from SFTP - receipt_delta, create dataframe out of it
+            # Add a column 'ins_dt' - current_date()
+            # Write dataframe in S3 partitioned by 'ins_dt'
+
+            pem_file_path = current_dir + "/../../" + app_secret["sftp_conf"]["pem"]
+
+            ol_txn_df = ut.read_from_sftp(spark, src_conf, app_secret, pem_file_path) \
+                .withColumn("ins_dt", current_date())
+
+            ol_txn_df.show(5, False)
+
+            ol_txn_df.write \
+                .partitionedBy("ins_dt") \
+                .parquet("s3:a//" + app_conf["s3_conf"]["s3_bucket"]+"/"+src)
+
+        elif src == 'CP':
+            # Read data from S3 - kc_extract_file, create dataframe out of it
+            # Add a column 'ins_dt' - current_date()
+            # Write dataframe in S3 partitioned by 'ins_dt'
+
+            finance_df = ut.read_from_s3(spark, src_conf) \
+                .withColumn("ins_dt", current_date())
+
+            finance_df.show(5, False)
+
+            finance_df.write \
+                .partitionedBy("ins_dt") \
+                .parquet("s3:a//" + app_conf["s3_conf"]["s3_bucket"]+"/"+src)
+
+        elif src == 'ADDR':
+            # Read data from MongoDB - , create dataframe out of it
+            # Add a column 'ins_dt' - current_date()
+            # Write dataframe in S3 partitioned by 'ins_dt'
+
+            addr_df = ut.read_from_mongodb(spark, src_conf, app_secret) \
+                .withColumn("ins_dt", current_date())
+
+            addr_df.show(5, False)
+
+            addr_df.write \
+                .partitionedBy("ins_dt") \
+                .parquet("s3:a//" + app_conf["s3_conf"]["s3_bucket"]+"/"+src)
+
+
