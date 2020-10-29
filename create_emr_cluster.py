d3973a99eb803723d42b4ae7a2ee1c282ca7e43f
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-29 18:59:28 +0530

    application yml and spark pem

---
+++ create_emr_cluster.py
@@ -0,0 +1,61 @@
+import boto3
+
+
+def lambda_handler(event, context):
+
+    client = boto3.client('emr',  region_name="eu-west-1")
+
+    instances = {
+        'MasterInstanceType': 'm3.xlarge',
+        'SlaveInstanceType': 'm3.xlarge',
+        'InstanceCount': 2,
+        'InstanceGroups': [],
+        'Ec2KeyName': 'spark',
+        'KeepJobFlowAliveWhenNoSteps': True,
+        'TerminationProtected': False,
+        'Ec2SubnetId': 'subnet-95ebd2f3',
+        'EmrManagedMasterSecurityGroup': 'sg-0ff6006f98c94b118',
+        'EmrManagedSlaveSecurityGroup':  'sg-0e169564c5f804ad7'
+    }
+
+    configurations = [
+        {
+            'Classification': 'yarn-site',
+            'Properties': {
+                'yarn.resourcemanager.scheduler.class': 'org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FairScheduler'
+            },
+            'Configurations': []
+        },
+        {
+            "Classification": "spark-env",
+            "Configurations": [
+                {
+                    "Classification": "export",
+                    "Properties": {
+                        "PYSPARK_PYTHON": "/usr/bin/python3"
+                    }
+                }
+            ]
+        }
+    ]
+
+    response = client.run_job_flow(
+        Name='PySpark Cluster',
+        LogUri='s3://sureshboddu/emr-logs',
+        ReleaseLabel='emr-5.30.0',
+        Instances=instances,
+        Configurations=configurations,
+        Steps=[],
+        BootstrapActions=[],
+        Applications=[
+            {'Name': 'Spark'},
+            {'Name': 'Zeppelin'},
+            {'Name': 'Ganglia'}
+        ],
+        VisibleToAllUsers=True,
+        ServiceRole='EMR_DefaultRole',
+        JobFlowRole='EMR_EC2_DefaultRole',
+        AutoScalingRole='EMR_AutoScaling_DefaultRole',
+        EbsRootVolumeSize=30
+    )
+    return response["JobFlowId"]
