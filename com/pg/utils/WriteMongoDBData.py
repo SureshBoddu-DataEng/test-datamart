b5302d5a2eca2ef303b2d0b68472734f3d5188a2
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-30 19:26:16 +0530

    changes

--- com/pg/utils/WriteMongoDBData.py
+++ com/pg/utils/WriteMongoDBData.py
@@ -13,13 +13,12 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read Files") \
-        .master('local[*]') \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel('ERROR')
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
@@ -42,4 +41,4 @@ if __name__ == '__main__':
 
     spark.stop()
 
-# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/files/json_df.py
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" com/pg/utils/WriteMongoDBData.py
