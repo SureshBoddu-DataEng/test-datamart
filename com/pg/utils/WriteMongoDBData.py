c55c64f53b9ef613f47ce9d3ddd60be7efdedcaf
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-30 19:52:50 +0530

    changes

--- com/pg/utils/WriteMongoDBData.py
+++ com/pg/utils/WriteMongoDBData.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read Files") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
+        .config("spark.mongodb.output.uri", app_secret["mongodb_config"]["uri"]) \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel('ERROR')
