c71b82553108301f3074db46a82817df315e2979
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-11-13 15:14:48 +0530

    change

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -1,16 +1,10 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_date
 import yaml
 import os.path
-import sys
 import com.pg.utils.utility as ut
 
 if __name__ == '__main__':
-
-    os.environ["PYSPARK_SUBMIT_ARGS"] = (
-        '--jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar"\
-         --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" pyspark-shell'
-    )
-
     # Create the SparkSession
     spark = SparkSession \
         .builder \
@@ -31,18 +25,19 @@ if __name__ == '__main__':
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
-    # print("Process Started.................................")
-    # src_list = app_conf["REGIS_DIM"]["sourceData"]
-    # print(src_list)
-    # for src in src_list:
-    #     print("src = "+src)
-    #     if src == 'CP':
-    #         print("Redading from S3   >>>>>>>")
-    #         txnDf = spark.read \
-    #             .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + app_conf["s3_conf"]["staging_dir"] + "/" + src) \
-    #             .repartition(5)
-    #
-    #         txnDf.show(5, False)
+
+    print("Process Started.................................")
+    src_list = app_conf["REGIS_DIM"]["sourceData"]
+    print(src_list)
+    for src in src_list:
+        print("src = "+src)
+        if src == 'CP':
+            print("Redading from S3   >>>>>>>")
+            txnDf = spark.read \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + app_conf["s3_conf"]["staging_dir"] + "/" + src) \
+                .repartition(5)
+
+            txnDf.show(5, False)
 
 
 
