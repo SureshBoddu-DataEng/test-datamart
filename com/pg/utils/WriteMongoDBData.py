9ea6de6d6b254fad480e63e37b749d9f881acf33
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-11-13 13:38:14 +0530

    change

--- com/pg/utils/WriteMongoDBData.py
+++ com/pg/utils/WriteMongoDBData.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read Files") \
-        .config("spark.mongodb.input.uri", 'mongodb://ec2-3-249-19-15.eu-west-1.compute.amazonaws.com:27017') \
-        .config("spark.mongodb.output.uri", 'mongodb://ec2-3-249-19-15.eu-west-1.compute.amazonaws.com:27017') \
+        .config("spark.mongodb.input.uri", 'mongodb://ec2-52-17-243-155.eu-west-1.compute.amazonaws.com:27017') \
+        .config("spark.mongodb.output.uri", 'mongodb://ec2-52-17-243-155.eu-west-1.compute.amazonaws.com:27017') \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel('ERROR')
