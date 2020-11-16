07d3ec59f59cfc27bd9f2a67efed99e1273d2146
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-11-16 17:37:27 +0530

    updates

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -112,4 +112,4 @@ if __name__ == '__main__':
                                       "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp",
                                       tgt_conf["tableName"])
 
-#spark-submit --packages "mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4,org.mongodb.spark:mongo-spark-connector_2.11:2.4.2" com/pg/target_data_loading.py
+#spark-submit --packages "mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4,org.mongodb.spark:mongo-spark-connector_2.11:2.4.2" --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/target_data_loading.py
